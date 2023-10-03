7bc0686c6c94a5ccbd01614e9ded6e6a650b4783
evan-forbes <evan.samuel.forbes@gmail.com>
2023-10-02 20:30:51 -0500

    chore: increase block part size to 1MB

--- types/params.go
+++ types/params.go
@@ -15,7 +15,7 @@ const (
 	MaxBlockSizeBytes = 500000000 // 500MB
 
 	// BlockPartSizeBytes is the size of one block part.
-	BlockPartSizeBytes uint32 = 65536 // 64kB
+	BlockPartSizeBytes uint32 = 1000000 // 1MB
 
 	// MaxBlockPartsCount is the maximum number of block parts.
 	MaxBlockPartsCount = (MaxBlockSizeBytes / BlockPartSizeBytes) + 1
