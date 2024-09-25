24e29218c7ce13198a001f9d5c96c2dc5c8da942
evan-forbes <evan.samuel.forbes@gmail.com>
2024-09-25 07:50:09 -0500

    chore: change value

--- mempool/cat/pool.go
+++ mempool/cat/pool.go
@@ -478,7 +478,7 @@ func (txmp *TxPool) ReapMaxBytesMaxGas(maxBytes, maxGas int64) types.Txs {
 		// 	continue
 		// }
 
-		if w.seenCount < 73 {
+		if w.seenCount < 25 {
 			txmp.logger.Error("too few seen to add to block!!", "peerCount", peerCount.Load(), "seen count", w.seenCount)
 			continue
 		} else {
