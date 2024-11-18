030ca99fe667b5b21ddfa3d066489cb3036ee59c
sweexordious <chamirachid1@gmail.com>
2024-11-18 14:04:55 +0100

    chore: remove log

--- p2p/load/mock_reactor.go
+++ p2p/load/mock_reactor.go
@@ -295,7 +295,6 @@ func (mr *MockReactor) FloodChannel(id p2p.ID, d time.Duration, chIDs ...byte) {
 		go func(d time.Duration, chID byte) {
 			start := time.Now()
 			for time.Since(start) < d {
-				mr.Logger.Error("sending data to peer", "peer", id)
 				mr.SendBytes(id, chID)
 			}
 		}(d, chID)
